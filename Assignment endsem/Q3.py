18222443af3bd447142756abb424ca15caa94e83
“Sankarshan-Mondal” <nmondalsanks@gmail.com>
2023-03-23 23:15:23 +0530

    edit

--- Assignment endsem/Q3.py
+++ Assignment endsem/Q3.py
@@ -3,31 +3,36 @@
 n = int(input('enter number of edges you want to enter:'))
 
 graph = []
+
 for i in range(n):
     l = input('Enter edges:').split(' ')
     if len(l)==2:
-        l = [int(i) for i in l]
-        graph.append(l)
+        k = [int(i) for i in l]
+        graph.append(k)
     else:
         print('Enter only two numbers')
         l = input('Enter edges:').split(' ')
 
+
 print(graph)
 
-#creating dictionary
-
+#creating set because we can get the keys of the dictionary from that
 set1 = []
 for i in graph:
     for j in i:
         set1.append(j)
 set1 = set(set1)
 
-print(set1)
+#print(set1) to check the keys
 
+#creating dictionary adjlst
 adjlst ={}
 
-for i in graph:
-    #j = []
-    for j in i:
-        for
+for k in set1:  #iterating the keys
+    a = []      #forming the output list
+    for i in graph:
+        if k in i:
+            a.append(i[1-i.index(k)]) #we are taking just the element of the list which is the output variable
+    adjlst[k]=a     #adding key-value pairs to the dictionary
 
+print('The dictionary of lists:', adjlst)
